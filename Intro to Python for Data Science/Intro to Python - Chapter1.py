b5e9645d3a5343f7423244fcaec69dda0b9f7da8
Amo Moloko <u14284783@tuks.co.za>
2018-02-07 12:19:46 +0200

    Chapter 1 Code

---
+++ Intro to Python for Data Science/Intro to Python - Chapter1.py
@@ -0,0 +1,89 @@
+  #Calculations with variables
+
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.1
+
+# Calculate result
+result = savings * factor ** 7
+
+# Print out result
+print(result)
+
+
+# Other Variable Types
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+
+
+#Operations with other types
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of savings and factor to year1
+year1 = savings * factor
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+
+#Type conversion
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+
+#Create a list
+# Area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+#Create list with different types
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+
