670b7cf2bd3f92203acde5c5f56ce7ae391e859c
Amo Moloko <u14284783@tuks.co.za>
2018-02-07 12:27:18 +0200

    Updates to chapters
    Removed chapter 2 code from chapter 1 :expressionless:

--- Intro to Python for Data Science/Intro to Python - Chapter1.py
+++ Intro to Python for Data Science/Intro to Python - Chapter1.py
@@ -58,32 +58,4 @@ pi_string = "3.1415926"
 pi_float = float(pi_string)
 
 
-#Create a list
-# Area variables (in square meters)
-hall = 11.25
-kit = 18.0
-liv = 20.0
-bed = 10.75
-bath = 9.50
-
-# Create list areas
-areas = [hall, kit, liv, bed, bath]
-
-# Print areas
-print(areas)
-
-#Create list with different types
-# area variables (in square meters)
-hall = 11.25
-kit = 18.0
-liv = 20.0
-bed = 10.75
-bath = 9.50
-
-# Adapt list areas
-areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
-
-# Print areas
-print(areas)
-
 
