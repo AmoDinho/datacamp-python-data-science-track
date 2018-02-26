e3fb12812278b19ebe41cfa3d430f7b9e75dddc6
Amo Moloko <u14284783@tuks.co.za>
2018-02-26 14:56:45 +0200

    Chapter Code
    Added the code for this chapter

---
+++ Cleaning Data in Python/Chapter 1 -Exploring your data.py
@@ -0,0 +1,79 @@
+#Chapter 1 - Exploring your data
+
+
+#Loading and viewing your data
+# Import pandas
+import pandas  as pd
+
+# Read the file into a DataFrame: df
+df = pd.read_csv('dob_job_application_filings_subset.csv')
+
+# Print the head of df
+print(df.head())
+
+# Print the tail of df
+print(df.tail())
+
+# Print the shape of df
+print(df.shape)
+
+# Print the columns of df
+print(df.columns)
+
+# Print the head and tail of df_subset
+print(df_subset.head())
+print(df_subset.tail())
+
+#Further diagnosis
+# Print the info of df
+print(df.info())
+
+# Print the info of df_subset
+print(df_subset.info())
+
+#Frequency counts for categorical data
+# Print the value counts for 'Borough'
+print(df['Borough'].value_counts(dropna=False))
+
+# Print the value_counts for 'State'
+print(df['State'].value_counts(dropna=False))
+
+# Print the value counts for 'Site Fill'
+print(df['Site Fill'].value_counts(dropna=False))
+
+#Visualizing single variables with histograms
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Plot the histogram
+df['Existing Zoning Sqft'].plot(kind='hist', rot=70, logx=True, logy=True)
+
+# Display the histogram
+plt.show()
+
+
+#Visualizing multiple variables with boxplots
+# Import necessary modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create the boxplot
+df.boxplot(column='initial_cost', by='Borough', rot=90)
+
+# Display the plot
+plt.show()
+
+#Visualizing multiple variables with scatter plots
+# Import necessary modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create and display the first scatter plot
+df.plot(kind='scatter', x='initial_cost', y='total_est_fee', rot=70)
+plt.show()
+
+# Create and display the second scatter plot
+df_subset .plot(kind='scatter', x='initial_cost', y='total_est_fee', rot=70)
+plt.show()
+
+
