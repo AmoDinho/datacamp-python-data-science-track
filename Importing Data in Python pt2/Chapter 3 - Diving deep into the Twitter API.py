7cbbf4396295137c568cb8e33d0af6f1cf770261
Amo Moloko <u14284783@tuks.co.za>
2018-02-26 14:42:07 +0200

    Final Code
    Added the code for the final chapter

---
+++ Importing Data in Python pt2/Chapter 3 - Diving deep into the Twitter API.py
@@ -0,0 +1,80 @@
+#Chapter 3 -Diving deep into the Twitter API
+
+#API Authentication
+# Import package
+import tweepy
+
+# Store OAuth authentication credentials in relevant variables
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+#Streaming tweets
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create you Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=['clinton', 'trump', 'sanders', 'cruz'])
+
+#Load and explore your Twitter data
+# Import package
+import json
+
+# String of path to file: tweets_data_path
+tweets_data_path = 'tweets.txt'
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data = []
+
+# Open connection to file
+tweets_file = open(tweets_data_path, "r")
+
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+
+# Close connection to file
+tweets_file.close()
+
+# Print the keys of the first tweet dict
+print(tweets_data[0].keys())
+
+#Twitter data to DataFrame
+# Import package
+import pandas as pd
+
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=['text', 'lang'])
+
+# Print head of DataFrame
+print(df.head())
+
+#A little bit of Twitter text analysis
+
+
+# Plotting your Twitter data
+# Import packages
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
+
