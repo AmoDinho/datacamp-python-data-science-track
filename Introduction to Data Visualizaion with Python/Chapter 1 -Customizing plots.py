aa2c5853107376c8bf28b20313fe93be22059ec9
Amo Moloko <u14284783@tuks.co.za>
2018-02-27 14:44:56 +0200

    Chapter 1 Code

--- Introduction to Data Visualizaion with Python/Chapter 1 -Customizing plots.py
+++ Introduction to Data Visualizaion with Python/Chapter 1 -Customizing plots.py
@@ -1,60 +1,221 @@
+
+#Chapter 1 - Customizing plots
 #-------------------------------------------------------------------------------------------#
 
+#Multiple plots on single axis
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
+
+# Display the plot
+plt.show()
 
 #-------------------------------------------------------------------------------------------#
+#Using axes()
+# Create plot axes for the first line plot
+plt.axes([0.05,0.05,0.425,0.9])
 
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
 
-#-------------------------------------------------------------------------------------------#
+# Create plot axes for the second line plot
+plt.axes([0.525,0.05,0.425,0.9])
 
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
 
-#-------------------------------------------------------------------------------------------#
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-#-------------------------------------------------------------------------------------------#
-
-
-
-#-------------------------------------------------------------------------------------------#
-
+# Display the plot
+plt.show()
 
 
 #-------------------------------------------------------------------------------------------#
 
+#Using subplot() (1)
+# Create a figure with 1x2 subplot and make the left subplot active
+plt.subplot(1,2,1)
+
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+plt.title('Physical Sciences')
+
+# Make the right subplot active in the current 1x2 subplot grid
+plt.subplot(1,2,1)
+
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
+plt.title('Computer Science')
+
+# Use plt.tight_layout() to improve the spacing between subplots
+plt.tight_layout()
+plt.show()
+
+#-------------------------------------------------------------------------------------------#
+#Using subplot() (2)
+# Create a figure with 2x2 subplot layout and make the top left subplot active
+plt.subplot(2, 2, 1) 
+
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+plt.title('Physical Sciences')
+
+# Make the top right subplot active in the current 2x2 subplot grid 
+plt.subplot(2, 2, 2)
+
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
+plt.title('Computer Science')
+
+# Make the bottom left subplot active in the current 2x2 subplot grid
+plt.subplot(2, 2, 3)
+
+# Plot in green the % of degrees awarded to women in Health Professions
+plt.plot(year, health, color='green')
+plt.title('Health Professions')
+
+# Make the bottom right subplot active in the current 2x2 subplot grid
+plt.subplot(2, 2, 4)
+
+# Plot in yellow the % of degrees awarded to women in the Education
+plt.plot(year, education, color='yellow')
+plt.title('Education')
+
+# Improve the spacing between subplots and display them
+plt.tight_layout()
+plt.show()
 
 
 #-------------------------------------------------------------------------------------------#
 
+#Using xlim(), ylim()
+# Plot the % of degrees awarded to women in Computer Science and the Physical Sciences
+plt.plot(year,computer_science, color='red') 
+plt.plot(year, physical_sciences, color='blue')
+
+# Add the axis labels
+plt.xlabel('Year')
+plt.ylabel('Degrees awarded to women (%)')
+
+# Set the x-axis range
+plt.xlim((1990,2010))
+
+# Set the y-axis range
+plt.ylim((0,50))
+
+# Add a title and display the plot
+plt.title('Degrees awarded to women (1990-2010)\nComputer Science (red)\nPhysical Sciences (blue)')
+plt.show()
+
+# Save the image as 'xlim_and_ylim.png'
+plt.show()
+plt.savefig('xlim_and_ylim.png')
+
+#-------------------------------------------------------------------------------------------#
+#Using axis()
+# Plot in blue the % of degrees awarded to women in Computer Science
+plt.plot(year,computer_science, color='blue')
+
+# Plot in red the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences,color='red')
+
+# Set the x-axis and y-axis limits
+plt.axis((1990,2010,0,50))
+
+# Show the figure
+plt.show()
+
+# Save the figure as 'axis_limits.png'
+plt.savefig('axis_limits.png')
 
 
 #-------------------------------------------------------------------------------------------#
 
+#Using legend()
+# Specify the label 'Computer Science'
+plt.plot(year, computer_science, color='red', label='Computer Science') 
+
+# Specify the label 'Physical Sciences' 
+plt.plot(year, physical_sciences, color='blue', label='Physical Sciences')
+
+# Add a legend at the lower center
+plt.legend(loc='lower center')
+
+# Add axis labels and title
+plt.xlabel('Year')
+plt.ylabel('Enrollment (%)')
+plt.title('Undergraduate enrollment of women')
+plt.show()
 
 
 #-------------------------------------------------------------------------------------------#
+#Using annotate()
+# Plot with legend as before
+plt.plot(year, computer_science, color='red', label='Computer Science') 
+plt.plot(year, physical_sciences, color='blue', label='Physical Sciences')
+plt.legend(loc='lower right')
+
+# Compute the maximum enrollment of women in Computer Science: cs_max
+cs_max = computer_science.max()
+
+# Calculate the year in which there was maximum enrollment of women in Computer Science: yr_max
+yr_max = year[computer_science.argmax()]
+
+# Add a black arrow annotation
+plt.annotate('Maximum', xy=(yr_max, cs_max), xytext=(yr_max+5, cs_max+5), arrowprops=dict(facecolor='black'))
+
+# Add axis labels and title
+plt.xlabel('Year')
+plt.ylabel('Enrollment (%)')
+plt.title('Undergraduate enrollment of women')
+plt.show()
+
+
+#-------------------------------------------------------------------------------------------#
+#Modifying styles
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Set the style to 'ggplot'
+plt.style.use('ggplot')
+
+# Create a figure with 2x2 subplot layout
+plt.subplot(2, 2, 1) 
+
+# Plot the enrollment % of women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+plt.title('Physical Sciences')
+
+# Plot the enrollment % of women in Computer Science
+plt.subplot(2, 2, 2)
+plt.plot(year, computer_science, color='red')
+plt.title('Computer Science')
+
+# Add annotation
+cs_max = computer_science.max()
+yr_max = year[computer_science.argmax()]
+plt.annotate('Maximum', xy=(yr_max, cs_max), xytext=(yr_max-1, cs_max-10), arrowprops=dict(facecolor='black'))
+
+# Plot the enrollmment % of women in Health professions
+plt.subplot(2, 2, 3)
+plt.plot(year, health, color='green')
+plt.title('Health Professions')
+
+# Plot the enrollment % of women in Education
+plt.subplot(2, 2, 4)
+plt.plot(year, education, color='yellow')
+plt.title('Education')
+
+# Improve spacing between subplots and display them
+plt.tight_layout()
+plt.show()
 
 
 
 #-------------------------------------------------------------------------------------------#
-
-
 
