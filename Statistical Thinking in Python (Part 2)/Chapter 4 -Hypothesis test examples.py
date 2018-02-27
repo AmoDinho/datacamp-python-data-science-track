757e5037a71777e4eeca05985bdffdcae4bbdf6c
Amo Moloko <u14284783@tuks.co.za>
2018-02-27 17:37:32 +0200

    Chapter 4 - Code

--- Statistical Thinking in Python (Part 2)/Chapter 4 -Hypothesis test examples.py
+++ Statistical Thinking in Python (Part 2)/Chapter 4 -Hypothesis test examples.py
@@ -1,25 +1,119 @@
 #----------------------------------------------------------------------------------#
+#Chapter - 4 - Hypothesis test examples
 
-
-#----------------------------------------------------------------------------------#
-
-
-#----------------------------------------------------------------------------------#
 
 
 
 #----------------------------------------------------------------------------------#
+#The vote for the Civil Rights Act in 1964
 
+# Construct arrays of data: dems, reps
+dems = np.array([True] * 153 + [False] * 91)
+reps = np.array([True] * 136 + [False] * 35)
+
+def frac_yay_dems(dems, reps):
+    """Compute fraction of Democrat yay votes."""
+    frac = np.sum(dems) / len(dems)
+    return frac
+
+# Acquire permutation samples: perm_replicates
+perm_replicates = draw_perm_reps(dems, reps, frac_yay_dems, 10000)
+
+# Compute and print p-value: p
+p = np.sum(perm_replicates <= 153/244) / len(perm_replicates)
+print('p-value =', p)
+
+
+#----------------------------------------------------------------------------------#
+#A time-on-website analog
+
+# Compute the observed difference in mean inter-no-hitter times: nht_diff_obs
+nht_diff_obs = diff_of_means(nht_dead, nht_live)
+
+# Acquire 10,000 permutation replicates of difference in mean no-hitter time: perm_replicates
+perm_replicates = draw_perm_reps(nht_dead, nht_live,
+                                 diff_of_means, size=10000)
+
+# Compute and print the p-value: p
+p = np.sum(perm_replicates <= nht_diff_obs) / len(perm_replicates)
+print('p-val =',p)
 
 
 #----------------------------------------------------------------------------------#
 
-
-#----------------------------------------------------------------------------------#
-
+#Simulating a null hypothesis concerning correlation
 
 
 #----------------------------------------------------------------------------------#
+#Hypothesis test on Pearson correlation
+# Compute observed correlation: r_obs
+r_obs = pearson_r(illiteracy, fertility)
+
+# Initialize permutation replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute illiteracy measurments: illiteracy_permuted
+    illiteracy_permuted = np.random.permutation(illiteracy)
+
+    # Compute Pearson correlation
+    perm_replicates[i] = pearson_r(illiteracy_permuted, fertility)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= r_obs) / len(perm_replicates)
+print('p-val =', p)
+
+
+#----------------------------------------------------------------------------------#
+
+#Do neonicotinoid insecticides have unintended consequences?
+# Compute x,y values for ECDFs
+x_control, y_control = ecdf(control)
+x_treated, y_treated = ecdf(treated)
+
+# Plot the ECDFs
+plt.plot(x_control, y_control, marker='.', linestyle='none')
+plt.plot(x_treated, y_treated, marker='.', linestyle='none')
+
+# Set the margins
+plt.margins(0.02)
+
+# Add a legend
+plt.legend(('control', 'treated'), loc='lower right')
+
+# Label axes and show plot
+plt.xlabel('millions of alive sperm per mL')
+plt.ylabel('ECDF')
+plt.show()
+
+
+#----------------------------------------------------------------------------------#
+#Bootstrap hypothesis test on bee sperm counts
+
+# Compute the difference in mean sperm count: diff_means
+diff_means = np.mean(control) - np.mean(treated)
+
+# Compute mean of pooled data: mean_count
+mean_count = np.mean(np.concatenate((control, treated)))
+
+# Generate shifted data sets
+control_shifted = control - np.mean(control) + mean_count
+treated_shifted = treated - np.mean(treated) + mean_count
+
+# Generate bootstrap replicates
+bs_reps_control = draw_bs_reps(control_shifted,
+                               np.mean, size=10000)
+bs_reps_treated = draw_bs_reps(treated_shifted,
+                               np.mean, size=10000)
+
+# Get replicates of difference of means: bs_replicates
+bs_replicates = bs_reps_control - bs_reps_treated
+
+# Compute and print p-value: p
+p = np.sum(bs_replicates >= np.mean(control) - np.mean(treated)) \
+            / len(bs_replicates)
+print('p-value =', p)
 
 
 #----------------------------------------------------------------------------------#
