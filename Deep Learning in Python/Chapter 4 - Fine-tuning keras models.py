99b105ef56eece80ac95fb95fef44414265182e2
Amo Moloko <u14284783@tuks.co.za>
2018-02-28 15:24:34 +0200

    Chapter 4 Code

--- Deep Learning in Python/Chapter 4 - Fine-tuning keras models.py
+++ Deep Learning in Python/Chapter 4 - Fine-tuning keras models.py
@@ -3,24 +3,166 @@
 #------------------------------------------------------------------------------------------------------------------------$
 #------------------------------------------------------------------------------------------------------------------------$
 
+#Chapter 4 - Fine-tuning keras models
+ 
+
+#------------------------------------------------------------------------------------------------------------------------$
+#Changing optimization parameters
+
+# Import the SGD optimizer
+from keras.optimizers import SGD
+
+# Create list of learning rates: lr_to_test
+lr_to_test = [.000001, 0.01, 1]
+
+# Loop over learning rates
+for lr in lr_to_test:
+    print('\n\nTesting model with learning rate: %f\n'%lr )
+    
+    # Build new model to test, unaffected by previous models
+    model = get_new_model()
+    
+    # Create SGD optimizer with specified learning rate: my_optimizer
+    my_optimizer = SGD(lr=lr)
+    
+    # Compile the model
+    model.compile(optimizer=my_optimizer, loss='categorical_crossentropy')
+    
+    # Fit the model
+    model.fit(predictors, target)
+
+#------------------------------------------------------------------------------------------------------------------------$
+#Evaluating model accuracy on validation dataset
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+input_shape = (n_cols,)
+
+# Specify the model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape = input_shape))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+hist = model.fit(predictors, target, validation_split=0.3)
+
+
+#------------------------------------------------------------------------------------------------------------------------$
+#Early stopping: Optimizing the optimization
+# Import EarlyStopping
+from keras.callbacks import EarlyStopping
+
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+input_shape = (n_cols,)
+
+# Specify the model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape = input_shape))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Define early_stopping_monitor
+early_stopping_monitor = EarlyStopping(patience=2)
+
+# Fit the model
+model.fit(predictors, target, epochs=30, validation_split=0.3, callbacks=[early_stopping_monitor])
+
+
 
 #------------------------------------------------------------------------------------------------------------------------$
 
+#Experimenting with wider networks
+# Define early_stopping_monitor
+early_stopping_monitor = EarlyStopping(patience=2)
+
+# Create the new model: model_2
+model_2 = Sequential()
+
+# Add the first and second layers
+model_2.add(Dense(100, activation='relu', input_shape=input_shape))
+model_2.add(Dense(100, activation='relu'))
+
+# Add the output layer
+model_2.add(Dense(2, activation='softmax'))
+
+# Compile model_2
+model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit model_1
+model_1_training = model_1.fit(predictors, target, epochs=15, validation_split=0.2, callbacks=[early_stopping_monitor], verbose=False)
+
+# Fit model_2
+model_2_training = model_2.fit(predictors, target, epochs=15, validation_split=0.2, callbacks=[early_stopping_monitor], verbose=False)
+
+# Create the plot
+plt.plot(model_1_training.history['val_loss'], 'r', model_2_training.history['val_loss'], 'b')
+plt.xlabel('Epochs')
+plt.ylabel('Validation score')
+plt.show()
+
 
 #------------------------------------------------------------------------------------------------------------------------$
+#Adding layers to a network
+# The input shape to use in the first hidden layer
+input_shape = (n_cols,)
+
+# Create the new model: model_2
+model_2 = Sequential()
+
+# Add the first, second, and third hidden layers
+model_2.add(Dense(50, activation='relu', input_shape=input_shape))
+model_2.add(Dense(50, activation='relu'))
+model_2.add(Dense(50, activation='relu'))
+
+# Add the output layer
+model_2.add(Dense(2, activation='softmax'))
+
+# Compile model_2
+model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit model 1
+model_1_training = model_1.fit(predictors, target, epochs=20, validation_split=0.4, callbacks=[early_stopping_monitor], verbose=False)
+
+# Fit model 2
+model_2_training = model_2.fit(predictors, target, epochs=20, validation_split=0.4, callbacks=[early_stopping_monitor], verbose=False)
+
+# Create the plot
+plt.plot(model_1_training.history['val_loss'], 'r', model_2_training.history['val_loss'], 'b')
+plt.xlabel('Epochs')
+plt.ylabel('Validation score')
+plt.show()
 
 
 #------------------------------------------------------------------------------------------------------------------------$
+#Building your own digit recognition model
+# Create the model: model
+model = Sequential()
+
+# Add the first hidden layer
+model.add(Dense(50, activation='relu', input_shape=(784,)))
+
+# Add the second hidden layer
+model.add(Dense(50, activation='relu'))
+
+# Add the output layer
+model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, y, validation_split=0.3)
 
 
-#------------------------------------------------------------------------------------------------------------------------$
-
-
-
-#------------------------------------------------------------------------------------------------------------------------$
-
-
-#------------------------------------------------------------------------------------------------------------------------$
 
 
 
