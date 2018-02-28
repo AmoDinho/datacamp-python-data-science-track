aaa1800a803248f7ec133c3352fa7ab2ec0ec74f
Amo Moloko <u14284783@tuks.co.za>
2018-02-28 15:41:20 +0200

    Chapter 1 Code

--- Network Analysis in Python (Part 1)/Chapter 1 - Introduction to networks.py
+++ Network Analysis in Python (Part 1)/Chapter 1 - Introduction to networks.py
@@ -1,3 +1,164 @@
 #Chapter 1 - Introduction to networks
 
 
+#---------=======================================================--------------%
+#Basic drawing of a network using NetworkX
+# Import necessary modules
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Draw the graph to screen
+nx.draw(T_sub)
+plt.show()
+
+#---------=======================================================--------------%
+
+#Queries on a graph
+# Use a list comprehension to get the nodes of interest: noi
+noi = [n for n, d in T.nodes(data=True) if d['occupation'] == 'scientist']
+
+# Use a list comprehension to get the edges of interest: eoi
+eoi = [(u, v) for u, v, d in T.edges(data=True) if d['date'] < date(2010, 1, 1)]
+
+
+#---------=======================================================--------------%
+# Specifying a weight on edges
+# Set the weight of the edge
+T.edge[1][10]['weight'] = 2
+
+# Iterate over all the edges (with metadata)
+for u, v, d in T.edges(data=True):
+
+    # Check if node 293 is involved
+    if 293 in [u, v]:
+    
+        # Set the weight to 1.1
+        T.edge[u][v]['weight'] = 1.1
+
+
+#---------=======================================================--------------%
+
+
+#Checking whether there are self-loops in the graph
+
+# Define find_selfloop_nodes()
+def find_selfloop_nodes(G):
+    """
+    Finds all nodes that have self-loops in the graph G.
+    """
+    nodes_in_selfloops = []
+    
+    # Iterate over all the edges of G
+    for u, v in G.edges():
+    
+        # Check if node u and node v are the same
+        if u == v:
+        
+            # Append node u to nodes_in_selfloops
+            nodes_in_selfloops.append(u)
+            
+    return nodes_in_selfloops
+
+# Check whether number of self loops equals the number of nodes in self loops
+assert T.number_of_selfloops() == len(find_selfloop_nodes(T))
+
+
+
+
+#---------=======================================================--------------%
+
+#Visualizing using Matrix plots
+# Import nxviz
+import nxviz as nv
+
+# Create the MatrixPlot object: m
+m = nv.MatrixPlot(T)
+
+# Draw m to the screen
+m.draw()
+
+# Display the plot
+plt.show()
+
+# Convert T to a matrix format: A
+A = nx.to_numpy_matrix(T)
+
+# Convert A back to the NetworkX form as a directed graph: T_conv
+T_conv = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
+
+# Check that the `category` metadata field is lost from each node
+for n, d in T_conv.nodes(data=True):
+    assert 'category' not in d.keys()
+#---------=======================================================--------------%
+
+#Visualizing using Circos plots
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import CircosPlot
+
+# Create the CircosPlot object: c
+c = CircosPlot(T)
+
+# Draw c to the screen
+c.draw()
+
+# Display the plot
+plt.show()
+
+#---------=======================================================--------------%
+
+
+
+#Visualizing using Arc plots
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import ArcPlot
+
+# Create the un-customized ArcPlot object: a
+a = ArcPlot(T)
+
+# Draw a to the screen
+a.draw()
+
+# Display the plot
+plt.show()
+
+# Create the customized ArcPlot object: a2
+a2 = ArcPlot(T, node_order='category', node_color='category')
+
+# Draw a2 to the screen
+a2.draw()
+
+# Display the plot
+plt.show()
+
+
+
+#---------=======================================================--------------%
+
+
+
+#---------=======================================================--------------%
+
+
+#---------=======================================================--------------%
+
+
+#---------=======================================================--------------%
+
+
+
+#---------=======================================================--------------%
+
+
+
+#---------=======================================================--------------%
+
+
+
+#---------=======================================================--------------%
+
+
+
+
+#---------=======================================================--------------%
