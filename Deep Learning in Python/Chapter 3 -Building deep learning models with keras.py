c9e681f77644e529d68199f3b3a71bcf88305cc8
Amo Moloko <u14284783@tuks.co.za>
2018-02-28 15:19:03 +0200

    Chapter 3 Code

--- Deep Learning in Python/Chapter 3 -Building deep learning models with keras.py
+++ Deep Learning in Python/Chapter 3 -Building deep learning models with keras.py
@@ -1,21 +1,122 @@
 #------------------------------------------------------------------------------------------------------------------------$
+#Chapter 3 - Building deep learning models with keras
+ 
 
 #------------------------------------------------------------------------------------------------------------------------$
 #------------------------------------------------------------------------------------------------------------------------$
+#Specifying a model
 
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+
+# Set up the model: model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(50, activation='relu', input_shape=(n_cols,)))
+
+# Add the second layer
+model.add(Dense(32, activation='relu'))
+
+# Add the output layer
+model.add(Dense(1))
 
 #------------------------------------------------------------------------------------------------------------------------$
 
+#Compiling the model
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Specify the model
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam',loss='mean_squared_error')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
 
 #------------------------------------------------------------------------------------------------------------------------$
 
+#Fitting the model
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Specify the model
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Fit the model
+model.fit(predictors,target)
 
 #------------------------------------------------------------------------------------------------------------------------$
+#Last steps in classification models
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+
+# Convert the target to categorical: target
+target = to_categorical(df.survived)
+
+# Set up the model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(32, activation='relu', input_shape=(n_cols,)))
+
+# Add the output layer
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='sgd', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
 
 
 #------------------------------------------------------------------------------------------------------------------------$
+#Making predictions
 
+# Specify, compile, and fit the model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(2, activation='softmax'))
+model.compile(optimizer='sgd', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+model.fit(predictors, target)
 
+# Calculate predictions: predictions
+predictions = model.predict(pred_data)
+
+# Calculate predicted probability of survival: predicted_prob_true
+predicted_prob_true = predictions[:,1]
+
+# print predicted_prob_true
+print(predicted_prob_true)
 
 #------------------------------------------------------------------------------------------------------------------------$
 
