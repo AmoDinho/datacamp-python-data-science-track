0a9edad76597e9cc5f5d92f4ad05962430ce6d22
Amo Moloko <u14284783@tuks.co.za>
2018-02-28 15:54:23 +0200

    Chapter 3 - Code

--- Network Analysis in Python (Part 1)/Chapter 3 - Structures.py
+++ Network Analysis in Python (Part 1)/Chapter 3 - Structures.py
@@ -1,24 +1,179 @@
 
 
-
-#---------=======================================================--------------%
-
-#---------=======================================================--------------%
+#Chapter 3 - Structures
 
 
 #---------=======================================================--------------%
+#Identifying triangle relationships
+from itertools import combinations
+
+# Define is_in_triangle() 
+def is_in_triangle(G, n):
+    """
+    Checks whether a node `n` in graph `G` is in a triangle relationship or not. 
+    
+    Returns a boolean.
+    """
+    in_triangle = False
+    
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+    
+        # Check if an edge exists between n1 and n2
+        if G.has_edge(n1, n2):
+            in_triangle = True
+            break
+    return in_triangle
+
+
+#---------=======================================================--------------%
+#Finding nodes involved in triangles
+
+from itertools import combinations
+
+# Write a function that identifies all nodes in a triangle relationship with a given node.
+def nodes_in_triangle(G, n):
+    """
+    Returns the nodes in a graph `G` that are involved in a triangle relationship with the node `n`.
+    """
+    triangle_nodes = set([n])
+    
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+    
+        # Check if n1 and n2 have an edge between them
+        if G.has_edge(n1, n2):
+        
+            # Add n1 to triangle_nodes
+            triangle_nodes.add(n1)
+            
+            # Add n2 to triangle_nodes
+            triangle_nodes.add(n2)
+            
+    return triangle_nodes
+    
+# Write the assertion statement
+assert len(nodes_in_triangle(T, 1)) == 35
+
+print(len(nodes_in_triangle(T, 1)))
+
+
+#---------=======================================================--------------%
+#Finding open triangles
+from itertools import combinations
+
+# Define node_in_open_triangle()
+def node_in_open_triangle(G, n):
+    """
+    Checks whether pairs of neighbors of node `n` in graph `G` are in an 'open triangle' relationship with node `n`.
+    """
+    in_open_triangle = False
+    
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+    
+        # Check if n1 and n2 do NOT have an edge between them
+        if not G.has_edge(n1, n2):
+        
+            in_open_triangle = True
+            
+            break
+            
+    return in_open_triangle
+
+# Compute the number of open triangles in T
+num_open_triangles = 0
+
+# Iterate over all the nodes in T
+for n in T.nodes():
+
+    # Check if the current node is in an open triangle
+    if node_in_open_triangle(T, n):
+    
+        # Increment num_open_triangles
+        num_open_triangles += 1
+        
+print(num_open_triangles)
+
 
 
 #---------=======================================================--------------%
 
 
-v
+#Finding all maximal cliques of size "n"
+# Define maximal_cliques()
+def maximal_cliques(G, size):
+    """
+    Finds all maximal cliques in graph `G` that are of size `size`.
+    """
+    mcs = []
+    for clique in nx.find_cliques(G):
+        if len(clique) == size:
+            mcs.append(clique)
+    return mcs
+
+# Check that there are 33 maximal cliques of size 3 in the graph T
+assert len(maximal_cliques(T, 3)) == 33
+
+
 
 
 #---------=======================================================--------------%
+#Subgraphs I
+nodes_of_interest = [29, 38, 42]  # provided.
+
+# Define get_nodes_and_nbrs()
+def get_nodes_and_nbrs(G, nodes_of_interest):
+    """
+    Returns a subgraph of the graph `G` with only the `nodes_of_interest` and their neighbors.
+    """
+    nodes_to_draw = []
+    
+    # Iterate over the nodes of interest
+    for n in nodes_of_interest:
+    
+        # Append the nodes of interest to nodes_to_draw
+        nodes_to_draw.append(n)
+        
+        # Iterate over all the neighbors of node n
+        for nbr in G.neighbors(n):
+        
+            # Append the neighbors of n to nodes_to_draw
+            nodes_to_draw.append(nbr)
+            
+    return G.subgraph(nodes_to_draw)
+
+# Extract the subgraph with the nodes of interest: T_draw
+T_draw = get_nodes_and_nbrs(T, nodes_of_interest)
+
+# Draw the subgraph to the screen
+nx.draw(T_draw)
+plt.show()
 
 #---------=======================================================--------------%
+## Subgraphs II
 
+# Extract the nodes of interest: nodes
+nodes = [n for n, d in T.nodes(data=True) if d['occupation'] == 'celebrity']
+
+# Create the set of nodes: nodeset
+nodeset = set(nodes)
+
+# Iterate over nodes
+for n in nodes:
+
+    # Compute the neighbors of n: nbrs
+    nbrs = T.neighbors(n)
+    
+    # Compute the union of nodeset and nbrs: nodeset
+    nodeset = nodeset.union(nbrs)
+
+# Compute the subgraph using nodeset: T_sub
+T_sub = T.subgraph(nodeset)
+
+# Draw T_sub to the screen
+nx.draw(T_sub)
+plt.show()
 
 
 #---------=======================================================--------------%
