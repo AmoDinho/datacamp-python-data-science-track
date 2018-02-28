ba109bb80ba29eb7ac606b3c2ba592c083df6fbc
Amo Moloko <u14284783@tuks.co.za>
2018-02-28 13:51:58 +0200

    Chapter 1 Code

--- Unsupervised Learning in Python/Chapter 1 - Clustering for dataset exploration.py
+++ Unsupervised Learning in Python/Chapter 1 - Clustering for dataset exploration.py
@@ -1,30 +1,175 @@
 #------------------------------------------------------------------------------------------------------------#
 
-#------------------------------------------------------------------------------------------------------------#
 
-
-#------------------------------------------------------------------------------------------------------------#
-
-
-
-#------------------------------------------------------------------------------------------------------------#
-
-
-#------------------------------------------------------------------------------------------------------------#
-
-
-#------------------------------------------------------------------------------------------------------------#
+#Chapter 1 - Clustering for dataset exploration
 
 
 
 #------------------------------------------------------------------------------------------------------------#
+#Clustering 2D points
+
+# Import KMeans
+from sklearn.cluster import KMeans
+
+# Create a KMeans instance with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Fit model to points
+model.fit(points)
+
+# Determine the cluster labels of new_points: labels
+labels = model.predict(new_points)
+
+# Print cluster labels of new_points
+print(labels)
+
 
 
 
 #------------------------------------------------------------------------------------------------------------#
 
+#Inspect your clustering
+
+
+# Import pyplot
+import matplotlib.pyplot as plt
+
+# Assign the columns of new_points: xs and ys
+xs = new_points[:,0]
+ys = new_points[:,1]
+
+# Make a scatter plot of xs and ys, using labels to define the colors
+plt.scatter(xs,ys,c=labels,alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = model.cluster_centers_
+
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+
+# Make a scatter plot of centroids_x and centroids_y
+plt.scatter(centroids_x,centroids_y,marker='D',s=50)
+plt.show()
+
+
+
+
 
 #------------------------------------------------------------------------------------------------------------#
+#How many clusters of grain?
+ks = range(1, 6)
+inertias = []
+
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)
+    
+    # Fit model to samples
+    model.fit(samples)
+    
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+    
+# Plot ks vs inertias
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
+
+
+#------------------------------------------------------------------------------------------------------------#
+#Evaluating the grain clustering
+# Create a KMeans model with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Use fit_predict to fit model and obtain cluster labels: labels
+labels = model.fit_predict(samples)
+
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'],df['varieties'])
+
+# Display ct
+print(ct)
+
+
+#------------------------------------------------------------------------------------------------------------#
+#Scaling fish data for clustering
+# Perform the necessary imports
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans 
+
+# Create scaler: scaler
+scaler = StandardScaler()
+
+# Create KMeans instance: kmeans
+kmeans = KMeans(n_clusters=4)
+
+# Create pipeline: pipeline
+pipeline = make_pipeline(scaler,kmeans)
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------#
+#Clustering the fish data
+# Import pandas
+import pandas as pd
+
+# Fit the pipeline to samples
+pipeline.fit(samples)
+
+# Calculate the cluster labels: labels
+labels = pipeline.predict(samples)
+
+# Create a DataFrame with labels and species as columns: df
+df = pd.DataFrame({'labels':labels,'species':species})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'],df['species'])
+
+# Display ct
+print(ct)
+
+
+
+#------------------------------------------------------------------------------------------------------------#
+#Clustering stocks using KMeans
+# Import Normalizer
+from sklearn.preprocessing import Normalizer
+
+# Create a normalizer: normalizer
+normalizer = Normalizer()
+
+# Create a KMeans model with 10 clusters: kmeans
+kmeans =  KMeans(n_clusters=10)
+
+# Make a pipeline chaining normalizer and kmeans: pipeline
+pipeline = make_pipeline(normalizer,kmeans)
+
+# Fit pipeline to the daily price movements
+pipeline.fit(movements)
+
+
+#------------------------------------------------------------------------------------------------------------#
+#Which stocks move together?
+# Import pandas
+import pandas as pd
+
+# Predict the cluster labels: labels
+labels = pipeline.predict(movements)
+
+# Create a DataFrame aligning labels and companies: df
+df = pd.DataFrame({'labels': labels, 'companies': companies})
+
+# Display df sorted by cluster label
+print(df.sort_values('labels'))
 
 
 
