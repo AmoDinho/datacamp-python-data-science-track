c002dff2b22c87dc15a93b91c738c97fa4531ae8
Amo Moloko <u14284783@tuks.co.za>
2018-02-28 16:54:30 +0200

    Chapter 2 Code

--- Natural Language Processing Fundamentals in Python/Chapter 2 -Simple topic identification.py
+++ Natural Language Processing Fundamentals in Python/Chapter 2 -Simple topic identification.py
@@ -1,21 +1,121 @@
 #---------------------------------------------------------------------------------------------------------------#
 
-
-
-#---------------------------------------------------------------------------------------------------------------#
-
-#---------------------------------------------------------------------------------------------------------------#
-
-
-#---------------------------------------------------------------------------------------------------------------#
+#Chapter 2 Simple topic identification
 
 
 
 #---------------------------------------------------------------------------------------------------------------#
+#Building a Counter with bag-of-words
+# Import Counter
+from collections import Counter
+
+# Tokenize the article: tokens
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [t.lower() for t in tokens]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(10))
 
 
 #---------------------------------------------------------------------------------------------------------------#
 
+#Text preprocessing practice
+# Import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer
+
+# Retain alphabetic words: alpha_only
+alpha_only = [t for t in lower_tokens if t.isalpha()]
+
+# Remove all stop words: no_stops
+no_stops = [t for t in alpha_only if t not in english_stops]
+
+# Instantiate the WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatize all tokens into a new list: lemmatized
+lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in no_stops]
+
+# Create the bag-of-words: bow
+bow = Counter(lemmatized)
+
+# Print the 10 most common tokens
+print(bow.most_common(10))
+
+#---------------------------------------------------------------------------------------------------------------#
+#Creating and querying a corpus with gensim
+# Import Dictionary
+from gensim.corpora.dictionary import Dictionary
+
+# Create a Dictionary from the articles: dictionary
+dictionary = Dictionary(articles)
+
+# Select the id for "computer": computer_id
+computer_id = dictionary.token2id.get("computerv")
+
+# Use computer_id with the dictionary to print the word
+print(dictionary.get(computer_id))
+
+# Create a MmCorpus: corpus
+corpus = [dictionary.doc2bow(article) for article in articles]
+
+# Print the first 10 word ids with their frequency counts from the fifth document
+print(corpus[4][:10])
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------#
+#Gensim bag-of-words
+# Save the fifth document: doc
+doc = corpus[4]
+
+# Sort the doc for frequency: bow_doc
+bow_doc = sorted(doc, key=lambda w: w[1], reverse=True)
+
+# Print the top 5 words of the document alongside the count
+for word_id, word_count in bow_doc[:5]:
+    print(dictionary.get(word_id), word_count)
+    
+# Create the defaultdict: total_word_count
+total_word_count = defaultdict(int)
+for word_id, word_count in itertools.chain.from_iterable(corpus):
+    total_word_count[word_id] += word_count
+
+# Create a sorted list from the defaultdict: sorted_word_count 
+sorted_word_count = sorted(total_word_count.items(), key=lambda w: w[1], reverse=True) 
+
+# Print the top 5 words across all documents alongside the count
+for word_id, word_count in sorted_word_count[:5]:
+    print(dictionary.get(word_id), word_count)
+
+
+#---------------------------------------------------------------------------------------------------------------#
+## Tf-iDf with Wikipedia
+# Import TfidfModel
+from gensim.models.tfidfmodel import TfidfModel
+
+# Create a new TfidfModel using the corpus: tfidf
+tfidf = TfidfModel(corpus)
+
+# Calculate the tfidf weights of doc: tfidf_weights
+tfidf_weights = tfidf[doc]
+
+# Print the first five weights
+print(tfidf_weights[:5])
+
+# Sort the weights from highest to lowest: sorted_tfidf_weights
+sorted_tfidf_weights = sorted(tfidf_weights, key=lambda w: w[1], reverse=True)
+
+# Print the top 5 weighted words
+for term_id, weight in sorted_tfidf_weights[:5]:
+    print(dictionary.get(term_id), weight)
 
 
 #---------------------------------------------------------------------------------------------------------------#
