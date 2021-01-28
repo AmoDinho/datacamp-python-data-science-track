2695e64ce35dc9e261f1ced24123d814490aadde
Shadab Hussain <shadab.entrepreneur@outlook.com>
2021-01-29 03:00:22 +0530

    Update Chapter 1 - Introduction to networks.py

--- Network Analysis in Python (Part 1)/Chapter 1 - Introduction to networks.py
+++ Network Analysis in Python (Part 1)/Chapter 1 - Introduction to networks.py
@@ -24,16 +24,16 @@ eoi = [(u, v) for u, v, d in T.edges(data=True) if d['date'] < date(2010, 1, 1)]
 #---------=======================================================--------------%
 # Specifying a weight on edges
 # Set the weight of the edge
-T.edge[1][10]['weight'] = 2
+T.edges[1, 10]['weight'] = 2
 
 # Iterate over all the edges (with metadata)
 for u, v, d in T.edges(data=True):
 
     # Check if node 293 is involved
     if 293 in [u, v]:
-    
+
         # Set the weight to 1.1
-        T.edge[u][v]['weight'] = 1.1
+        T.edges[u, v]['weight'] = 1.1
 
 
 #---------=======================================================--------------%
